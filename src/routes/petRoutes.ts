8cd9baf37fa6551382362aa849b88220efc470ff
vitorbachini <vbachiniw@gmail.com>
2024-05-30 13:43:49 -0300

    feat: add validator to routes

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { validateReq } from '../middlewares/validationMiddleware';
+import { petValidator } from '../validation/pet';
 
 export const petRouter = Router();
 
-petRouter.route('/pet/:tutorId').post()
-petRouter.route('/pet/:petId/tutor/:tutorId').put()
-petRouter.route('/pet/:petId/tutor/:tutorId').delete()
+petRouter.route('/pet/:tutorId').post(validateReq(petValidator));
+petRouter.route('/pet/:petId/tutor/:tutorId').put(validateReq(petValidator));
+petRouter.route('/pet/:petId/tutor/:tutorId').delete();
--- src/routes/tutorRoutes.ts
+++ src/routes/tutorRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { validateReq } from '../middlewares/validationMiddleware';
+import { tutorValidator } from '../validation/tutor';
 
 export const tutorRouter = Router();
 
-tutorRouter.route('/tutors').get()
-tutorRouter.route('/tutor').post()
-tutorRouter.route('/tutor/:id').put()
-tutorRouter.route('/tutor/:id').delete()
+tutorRouter.route('/tutors').get();
+tutorRouter.route('/tutor').post(validateReq(tutorValidator));
+tutorRouter.route('/tutor/:id').put(validateReq(tutorValidator));
+tutorRouter.route('/tutor/:id').delete();
