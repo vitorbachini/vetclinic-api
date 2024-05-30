aa90089cd8838d6a2a8e7a08d38da343ed9224df
vitorbachini <vbachiniw@gmail.com>
2024-05-30 13:32:30 -0300

    feat: create validation middleware

---
+++ src/middlewares/validationMiddleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
+
+export const validateReq = (schema: Joi.ObjectSchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+        next();
+    };
+};
