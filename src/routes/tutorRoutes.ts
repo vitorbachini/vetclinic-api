98d7da02294616b7cfe0cdc46052d1bcd5bdad5e
vitorbachini <vbachiniw@gmail.com>
2024-05-31 12:32:48 -0300

    feat: added controllers to tutor routes

--- src/routes/tutorRoutes.ts
+++ src/routes/tutorRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { validateReq } from '../middlewares/validationMiddleware';
 import { tutorValidator } from '../validation/tutor';
+import { getAllTutors } from '../controllers/tutorControllers/getTutors';
+import { createTutor } from '../controllers/tutorControllers/createTutor';
+import { updateTutor } from '../controllers/tutorControllers/updateTutor';
+import { deleteTutor } from '../controllers/tutorControllers/deleteTutor';
 
 export const tutorRouter = Router();
 
-tutorRouter.route('/tutors').get();
-tutorRouter.route('/tutor').post(validateReq(tutorValidator));
-tutorRouter.route('/tutor/:id').put(validateReq(tutorValidator));
-tutorRouter.route('/tutor/:id').delete();
+tutorRouter.route('/tutors').get(getAllTutors);
+tutorRouter.route('/tutor').post(validateReq(tutorValidator), createTutor);
+tutorRouter.route('/tutor/:id').put(validateReq(tutorValidator), updateTutor);
+tutorRouter.route('/tutor/:id').delete(deleteTutor);
