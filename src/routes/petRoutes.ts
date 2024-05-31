04af62e81e6617f5dc46918f34b42a11202d53b0
vitorbachini <vbachiniw@gmail.com>
2024-05-31 12:34:08 -0300

    feat: added pet controllers to routes

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { validateReq } from '../middlewares/validationMiddleware';
 import { petValidator } from '../validation/pet';
+import { createPet } from '../controllers/petControllers/createPet';
+import { updatePet } from '../controllers/petControllers/updatePet';
+import { deletePet } from '../controllers/petControllers/deletePet';
+
 
 export const petRouter = Router();
 
-petRouter.route('/pet/:tutorId').post(validateReq(petValidator));
-petRouter.route('/pet/:petId/tutor/:tutorId').put(validateReq(petValidator));
-petRouter.route('/pet/:petId/tutor/:tutorId').delete();
+petRouter.route('/pet/:tutorId').post(validateReq(petValidator), createPet);
+petRouter.route('/pet/:petId/tutor/:tutorId').put(validateReq(petValidator), updatePet);
+petRouter.route('/pet/:petId/tutor/:tutorId').delete(deletePet);
