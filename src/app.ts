5669650486842b7f8283aef1affc2e10227722dd
vitorbachini <vbachiniw@gmail.com>
2024-06-04 13:50:55 -0300

    feat: created swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { tutorRouter } from './routes/tutorRoutes';
 import { petRouter } from './routes/petRoutes';
 
+import swaggerDocs from './swagger.json';
+
 export const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/api/v1', tutorRouter);
 app.use('/api/v1', petRouter);
--- src/controllers/tutorControllers/getTutors.ts
+++ src/controllers/tutorControllers/getTutors.ts
@@ -4,7 +4,7 @@ import Tutor from '../../models/Tutor';
 export const getAllTutors = async (req: Request, res: Response) => {
     try {
         const tutors = await Tutor.find().populate('pets');
-        res.status(201).json(tutors);
+        res.status(200).json(tutors);
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
     }
